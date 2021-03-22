42d17d91a171ff60b9684c086ff120d85d08adc8
Michael DeWitt <me.dewitt.jr@gmail.com>
2021-03-22 06:43:06 -0400

    graph

--- manual/functions.jl
+++ manual/functions.jl
@@ -1,10 +1,14 @@
 # function syntax
 using Distributions
 
-function f(N, mu, sd) :: Float64
+function f(N, mu, sd)
     a = Normal(mu,sd)
     x = rand(a, N)
     return x
 end
 
-f(500)
+f(100, 2., 4.)
+
+using Plots
+x = 1:10; y = rand(10); # These are the plotting data 
+plot(x,y, label="my label")
